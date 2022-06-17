75bc6d369fa27d36ac81e44f6a6dc1a100b4054d
Gabriele Piaggesi <piaggesigabriele@gmail.com>
2022-06-17 17:56:03 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../global.css';
+import classes from './Header.module.css';
+
+const Header = () => {
+    return (
+        <div className="flex fRow jBet pad5">
+            <div>Azienda</div>
+            <div>03:00</div>
+        </div>
+    )
+};
+
+export default Header;
